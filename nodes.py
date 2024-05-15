d31744f071e39222e00359fc9921a0e7a6f8be06
alisson-anjos <alissonerdx@gmail.com>
2024-05-15 18:39:32 +0100

    change llava describer and added ollama describer

--- nodes.py
+++ nodes.py
@@ -5,60 +5,10 @@ from PIL import Image
 from io import BytesIO
 import base64
 
-class LlavaDescriber:
+class OllamaUtil:
     def __init__(self):
         pass
     
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "optional": {
-              'system_context': ("STRING", {
-                  "forceInput": True
-              }),
-            },
-            "required": {
-                "image": ("IMAGE",),  
-                "model": (["llava:7b-v1.6", "llava:13b-v1.6", "llava:34b-v1.6"],),
-                "api_host": ("STRING", {
-                  "default": "http://localhost:11434"
-                }),
-                "temperature": ("FLOAT", {
-                    "min": 0,
-                    "max": 1,
-                    "step": 0.1,
-                    "default": 0.2
-                }),
-                "seed_number": ("INT", {
-                    "min": -1,
-                    "max": 0xffffffffffffffff,
-                    "step": 1,
-                    "default": 0
-                }),
-                "max_tokens": ("INT", {
-                    "step": 10,
-                    "default": 200
-                }),
-                "keep_model_alive": ("INT", {
-                    "step": 1,
-                    "default": -1
-                }),
-                "prompt": ("STRING", {
-                    "default": "Return a list of danbooru tags for this image, formatted as lowercase, separated by commas.",
-                    "multiline": True
-                })
-            },
-        }
-
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("description",)
-
-    FUNCTION = "process_image"
-
-    OUTPUT_NODE = True
-
-    CATEGORY = "image"
-
     def tensor_to_image(self, tensor):
         tensor = tensor.cpu()
     
@@ -66,7 +16,7 @@ class LlavaDescriber:
     
         image = Image.fromarray(image_np, mode='RGB')
         return image
-    
+        
     def image_to_bytes(self, image: Image.Image):
         with BytesIO() as output:
             image.save(output, format="PNG")
@@ -91,42 +41,199 @@ class LlavaDescriber:
                 bars[digest].update(completed - bars[digest].n)
 
             current_digest = digest
-  
-  
-    def process_image(self, image, model, api_host, temperature, seed_number, max_tokens, keep_model_alive, prompt, system_context=None):
-        print('Converting Tensor to Image')
-        img = self.tensor_to_image(image)
-        
-        print('Converting Image to bytes')
-        img_bytes = self.image_to_bytes(img)
-        
-        print('Generating Description from Image')
-        full_response = ""
+            
+class LlavaDescriber:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": (["llava:7b-v1.6", "llava:13b-v1.6", "llava:34b-v1.6"],),
+                "api_host": ("STRING", {
+                  "default": "http://localhost:11434"
+                }),
+                "timeout": ("INT", {
+                    "min": 0,
+                    "max": 0xffffffffffffffff,
+                    "step": 1,
+                    "default": 300
+                }),
+                "temperature": ("FLOAT", {
+                    "min": 0,
+                    "max": 1,
+                    "step": 0.1,
+                    "default": 0.2
+                }),
+                "seed_number": ("INT", {
+                    "min": -1,
+                    "max": 0xffffffffffffffff,
+                    "step": 1,
+                    "default": 0
+                }),
+                "max_tokens": ("INT", {
+                    "step": 10,
+                    "default": 200
+                }),
+                "keep_model_alive": ("INT", {
+                    "step": 1,
+                    "default": -1
+                }),
+                "image": ("IMAGE",),  
+                "system_context": ("STRING", {
+                    "multiline": True,
+                    "default": """You are an assistant who describes the content and composition of images. 
+                    Describe only what you see in the image, not what you think the image is about.Be factual and literal. 
+                    Do not use metaphors or similes. 
+                    Be concise.""",
+                    "title":"system"
+                }),
+                "prompt": ("STRING", {
+                    "default": "Return a list of danbooru tags for this image, formatted as lowercase, separated by commas.",
+                    "multiline": True
+                })
+            },
+        }
 
-        # use the passed system context if it exists, otherwise use the default
-        if system_context is None:
-            system_context = """You are an assistant who describes the content and composition of images. 
-            Describe only what you see in the image, not what you think the image is about.Be factual and literal. 
-            Do not use metaphors or similes. Be concise.
-            """
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("description",)
 
-        print('System Context: "{}"'.format(system_context))
+    FUNCTION = "ollama_image_describe"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "Ollama"
+
+    def ollama_image_describe(self, model, api_host, timeout, temperature, seed_number, max_tokens, keep_model_alive, prompt, system_context, image):
         
-        client = Client(api_host, timeout=300)
+        client = Client(api_host, timeout=timeout)
+        
+        ollama_util = OllamaUtil()
+        
         models = [model_l['name'] for model_l in client.list()['models']]
         
         if model not in models:
-            self.pull_model(model, client)
+            print(f"Downloading model: {model}")
+            ollama_util.pull_model(model, client)
             
-        full_response = client.generate(model=model, system=system_context, prompt=prompt, images=[img_bytes], keep_alive=keep_model_alive, stream=False, options={
+        print('System Context: "{}"'.format(system_context))
+        
+        full_response = ""
+                
+        print('Converting Tensor to Image')
+        img = ollama_util.tensor_to_image(image)
+    
+        print('Converting Image to bytes')
+        img_bytes = ollama_util.image_to_bytes(img)
+    
+        print('Generating Description from Image')
+        full_response =  client.generate(model=model, system=system_context, prompt=prompt, images=[img_bytes], keep_alive=keep_model_alive, stream=False, options={
                 'num_predict': max_tokens,
                 'temperature': temperature,
                 'seed': seed_number
         })
         
-        print('Finalizing')
+        print('Finalized')
         return (full_response['response'], )
 
+
+
+class OllamaDescriber:
+    
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        
+        
+        return {
+            "required": {
+                "model": (["gemma:2b", 
+                           "gemma:7b",
+                           "llama2:7b", 
+                           "llama2:13b", 
+                           "llama2:70b", 
+                           "llama3:8b", 
+                           "llama3:70b",
+                           "mistral:7b"],),
+                "api_host": ("STRING", {
+                  "default": "http://localhost:11434"
+                }),
+                "timeout": ("INT", {
+                    "min": 0,
+                    "max": 0xffffffffffffffff,
+                    "step": 1,
+                    "default": 300
+                }),
+                "temperature": ("FLOAT", {
+                    "min": 0,
+                    "max": 1,
+                    "step": 0.1,
+                    "default": 0.2
+                }),
+                "seed_number": ("INT", {
+                    "min": -1,
+                    "max": 0xffffffffffffffff,
+                    "step": 1,
+                    "default": 0
+                }),
+                "max_tokens": ("INT", {
+                    "step": 10,
+                    "default": 200
+                }),
+                "keep_model_alive": ("INT", {
+                    "step": 1,
+                    "default": -1
+                }),
+                "system_context": ("STRING", {
+                    "multiline": True,
+                    "default": """You are an assistant who describes content about art.
+Describe your knowledge of all types of visual art, your objective is to describe in detail what is requested, be concise.
+                    """,
+                    "title":"system"
+                }),
+                "prompt": ("STRING", {
+                    "default": "In one row, return a list, formatted in lowercase letters, separated by commas, describing Little Red Riding Hood.",
+                    "multiline": True
+                })
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("description",)
+
+    FUNCTION = "ollama_describe"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "Ollama"
+
+    def ollama_describe(self, model, api_host, timeout, temperature, seed_number, max_tokens, keep_model_alive, prompt, system_context):
+        client = Client(api_host, timeout=timeout)
+        ollama_util = OllamaUtil()
+        
+        models = [model_l['name'] for model_l in client.list()['models']]
+        
+        if model not in models:
+            print(f"Downloading model: {model}")
+            ollama_util.pull_model(model, client)
+            
+        print('System Context: "{}"'.format(system_context))
+        
+        full_response = ""
+                
+        print('Generating Response')
+        full_response =  client.generate(model=model, system=system_context, prompt=prompt, keep_alive=keep_model_alive, stream=False, options={
+                'num_predict': max_tokens,
+                'temperature': temperature,
+                'seed': seed_number
+        })
+        
+        print('Finalized')
+        return (full_response['response'], )
+    
     #@classmethod
     #def IS_CHANGED(s, image, string_field, int_field, float_field, print_to_screen):
     #    return ""
@@ -138,10 +245,12 @@ class LlavaDescriber:
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "LLaVaDescriber": LlavaDescriber
+    "LLaVaDescriber": LlavaDescriber,
+    "OllamaDescriber": OllamaDescriber
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LLaVaDescriber": "🌋 LLaVa Describer 🦙"
+    "LLaVaDescriber": "🌋 LLaVa Describer 🦙",
+    "OllamaDescriber": "🦙 Ollama Describer 🦙"
 }
